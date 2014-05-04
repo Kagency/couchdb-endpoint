dfe32c189d0cb33acb4b93d603fd93f79c7b8775
Kore Nordmann <mail@kore-nordmann.de>
2014-05-04 13:59:01 +0200

    Pass recorded dumps as SF2 request / response to tests

--- tests/Kagency/CouchdbEndpoint/IntegrationTest.php
+++ tests/Kagency/CouchdbEndpoint/IntegrationTest.php
@@ -2,9 +2,73 @@
 
 namespace Kagency\CouchdbEndpoint;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
-    public function testReplayReplication()
+    /**
+     * Get fixtures
+     *
+     * @return array
+     */
+    public function getFixtures()
+    {
+        $dumps = array();
+        $decoder = new \TNetstring_Decoder();
+        foreach (glob(__DIR__ . '/_fixtures/*.tns') as $fixtureFile) {
+            $dumps[] = array(
+                array_map(
+                    function (array $interaction) {
+                        return array(
+                            'request' => Request::create(
+                                $interaction['request']['path'],
+                                $interaction['request']['method'],
+                                array(),
+                                array(),
+                                array(),
+                                $this->mapHeaders($interaction['request']['headers']),
+                                $interaction['request']['content']
+                            ),
+                            'response' => Response::create(
+                                $interaction['response']['content'],
+                                $interaction['response']['code'],
+                                $this->mapHeaders($interaction['response']['headers'])
+                            ),
+                        );
+                    },
+                    $decoder->decode(file_get_contents($fixtureFile))
+                )
+            );
+        }
+
+        return $dumps;
+    }
+
+    /**
+     * Map headers
+     *
+     * Maps HTTP headers from the real names to the naems PHP would use in the
+     * SERVER array, so that Symfony2 can map them back.
+     *
+     * @param array $headers
+     * @return array
+     */
+    protected function mapHeaders(array $headers)
+    {
+        $phpHeaders = array();
+        foreach ($headers as $headerPair) {
+            list($name, $value) = $headerPair;
+            $phpHeaders['HTTP_' . str_replace('-', '_', strtoupper($name))] = $value;
+        }
+
+        return $phpHeaders;
+    }
+
+    /**
+     * @dataProvider getFixtures
+     */
+    public function testReplayReplication(array $dumps)
     {
         $this->markTestIncomplete("@TODO: Implement Test.");
     }
