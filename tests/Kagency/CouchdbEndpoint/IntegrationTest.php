e01b2a2a7b61322e4df39dbdfb1c425c663d16fa
Kore Nordmann <mail@kore-nordmann.de>
2014-05-04 10:00:35 +0200

    tubbed PHPUnit tests

---
+++ tests/Kagency/CouchdbEndpoint/IntegrationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kagency\CouchdbEndpoint;
+
+class IntegrationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReplayReplication()
+    {
+        $this->markTestIncomplete("@TODO: Implement Test.");
+    }
+}
