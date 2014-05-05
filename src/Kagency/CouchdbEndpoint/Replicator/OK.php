171d01f37e3db0b00d6649f044989a0dbdcdfaac
Kore Nordmann <mail@kore-nordmann.de>
2014-05-05 19:05:23 +0200

    Added: OK struct

---
+++ src/Kagency/CouchdbEndpoint/Replicator/OK.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kagency\CouchdbEndpoint\Replicator;
+
+use Kagency\CouchdbEndpoint\Struct;
+
+/**
+ * Class: OK
+ *
+ * CouchDB success message
+ *
+ * @version $Revision$
+ */
+class OK
+{
+    /**
+     * Everything is OK
+     *
+     * @var bool
+     */
+    public $ok = true;
+}
