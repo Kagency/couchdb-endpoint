4ff0096920bc0b051cd2e33fd65adb7a1c280f27
Kore Nordmann <mail@kore-nordmann.de>
2014-05-09 18:34:28 +0200

    Switched to a SF2 DIC base

--- src/Kagency/CouchdbEndpoint/Container.php
+++ src/Kagency/CouchdbEndpoint/Container.php
@@ -2,20 +2,68 @@
 
 namespace Kagency\CouchdbEndpoint;
 
-class Container extends \Pimple
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\Config\FileLocator;
+
+class Container
 {
     /**
-     * __construct
+     * @var \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    public $container;
+
+    /**
+     * @var string
+     */
+    private $basedir;
+
+    /**
+     * @param string $configuration
+     * @param string|null $basedir
+     * @param string|null $cachedir
+     */
+    public function __construct($basedir = null)
+    {
+        $this->basedir = $basedir ?: __DIR__ . '/../../config';
+    }
+
+    /**
+     * Get service from container
      *
+     * @param $identifier
+     * @return mixed
+     */
+    public function get($identifier)
+    {
+        $this->compile();
+        return $this->container->get($identifier);
+    }
+
+    /**
      * @return void
      */
-    public function __construct()
+    private function compile()
     {
-        parent::__construct();
+        if ($this->container !== null) {
+            return;
+        }
 
-        $this['storage'] = new Storage();
-        $this['replicator'] = new Replicator(
-            $this['storage']
+        $this->container = new ContainerBuilder();
+
+        $this->parser = new \Symfony\Component\Yaml\Parser();
+        $configuration = $this->parser->parse(file_get_contents($this->basedir . '/config.yml')) ?: array();
+        foreach ($configuration as $name => $value) {
+            $this->container->setParameter($name, $value);
+        }
+
+        $loader = new XmlFileLoader(
+            $this->container,
+            new FileLocator($this->basedir)
         );
+        $loader->load('services.xml');
+
+        $this->container->compile();
     }
 }
