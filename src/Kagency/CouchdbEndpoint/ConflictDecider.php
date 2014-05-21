a8254025bf74986c21489c2b04f5e7988d1069ba
Kore Nordmann <mail@kore-nordmann.de>
2014-05-21 17:16:30 +0200

    Implemented very simple conflict decider
    Until no it mimics CouchDBs behaviour for a very simple use case. I
    guess it is quite important to exactly mimic CouchDBs behaviour here.
    Otherwise storages might not really be eventual consistent. We might
    want to check CouchDBs, PouchDBs and TocuhDBs behaviour here.

--- src/Kagency/CouchdbEndpoint/ConflictDecider.php
+++ src/Kagency/CouchdbEndpoint/ConflictDecider.php
@@ -5,6 +5,24 @@ namespace Kagency\CouchdbEndpoint;
 class ConflictDecider
 {
     /**
+     * Revision calculator
+     *
+     * @var RevisionCalculator
+     */
+    private $revisionCalculator;
+
+    /**
+     * __construct
+     *
+     * @param RevisionCalculator $revisionCalculator
+     * @return void
+     */
+    public function __construct(RevisionCalculator $revisionCalculator)
+    {
+        $this->revisionCalculator = $revisionCalculator;
+    }
+
+    /**
      * Select conflict winner
      *
      * @param array $doc1
@@ -13,6 +31,21 @@ class ConflictDecider
      */
     public function select(array $doc1, array $doc2)
     {
+        $doc1Sequence = $this->revisionCalculator->getSequence($doc1['_rev']);
+        $doc2Sequence = $this->revisionCalculator->getSequence($doc2['_rev']);
+
+        if (!$doc1Sequence !== $doc2Sequence) {
+            return $doc1Sequence > $doc2Sequence ? $doc1 : $doc2;
+        }
+
+        $doc1Size = strlen(json_encode($doc1));
+        $doc2Size = strlen(json_encode($doc2));
+
+        if (!$doc1Size !== $doc2Size) {
+            return $doc1Size > $doc2Size ? $doc1 : $doc2;
+        }
+
+        // We do not care
         return $doc1;
     }
 }
