1af031701cb8a946332bf50dc4f6dd0d9ef77cbb
Kore Nordmann <mail@kore-nordmann.de>
2014-05-23 16:43:42 +0200

    Improved conflic decider
    Does not mark conflicted documents yet

--- src/Kagency/CouchdbEndpoint/ConflictDecider.php
+++ src/Kagency/CouchdbEndpoint/ConflictDecider.php
@@ -34,18 +34,36 @@ class ConflictDecider
         $doc1Sequence = $this->revisionCalculator->getSequence($doc1['_rev']);
         $doc2Sequence = $this->revisionCalculator->getSequence($doc2['_rev']);
 
-        if (!$doc1Sequence !== $doc2Sequence) {
+        // We assume there is a clear winner in this case. This might not be
+        // correct, if one endpoint had more changes then the other other one.
+        if ($doc1Sequence !== $doc2Sequence) {
             return $doc1Sequence > $doc2Sequence ? $doc1 : $doc2;
         }
 
+        // We enter conflict zone:
         $doc1Size = strlen(json_encode($doc1));
         $doc2Size = strlen(json_encode($doc2));
 
-        if (!$doc1Size !== $doc2Size) {
-            return $doc1Size > $doc2Size ? $doc1 : $doc2;
+        if ($doc1Size !== $doc2Size) {
+            return $this->flagWinner(
+                $doc1Size > $doc2Size ? $doc1 : $doc2,
+                $doc1Size > $doc2Size ? $doc2 : $doc1
+            );
         }
 
-        // We do not care
-        return $doc1;
+        // @TODO: Just select one of the documents randomly. This requires more
+        // checking.
+        return $this->flagWinner($doc1, $doc2);
+    }
+
+    /**
+     * Flag and return winner document
+     *
+     * @param array $winner
+     * @param array $looser
+     * @return array
+     */
+    protected function flagWinner(array $winner, array $looser)
+    {
     }
 }
